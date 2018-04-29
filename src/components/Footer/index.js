3108f13f2b15f1fb7758ca77f55a04c2ee80c6a3
Will Harrison <willharrison@gmail.com>
2018-04-29 15:07:45 -0700

    footer with social

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,35 +1,54 @@
 import React from 'react';
 import styled from 'styled-components'
 import Link from 'gatsby-link'
-import { Facebook, Twitter } from 'react-svg-logos'
+import { FaFacebookSquare, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/lib/fa'
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterContact>
-        <a href='tel:808-722-5492'>808-722-5492</a>
-        <a href='mailto:info@localdemoservice.com'>info@localdemoservice.com</a>
-      </FooterContact>
-      <FooterLinks>
-        <Link to={'/about'}>About</Link>
-        <Link to={'/locations'}>Locations</Link>
-        <Link to={'/meet-the-team'}>Meet The Team</Link>
-        <Link to={'/careers'}>Careers</Link>
-      </FooterLinks>
-      <FooterSocial>
-        <Facebook style={{ height: 100 }} />
-      </FooterSocial>
+      <FooterWidth>
+        <FooterContact>
+          <a href='tel:808-722-5492'>808-722-5492</a>
+          <a href='mailto:info@localdemoservice.com'>info@localdemoservice.com</a>
+        </FooterContact>
+        <FooterLinks>
+          <Link to={'/about'}>About</Link>
+          <Link to={'/locations'}>Locations</Link>
+          <Link to={'/meet-the-team'}>Meet The Team</Link>
+          <Link to={'/careers'}>Careers</Link>
+        </FooterLinks>
+        <FooterSocial>
+          <a href='https://facebook.com'><FaFacebookSquare size={50} svg /></a>
+          <a href=''><FaLinkedin size={50} svg /></a>
+          <a href=''><FaTwitter size={50} svg /></a>
+          <a href=''><FaInstagram size={50} svg /></a>
+        </FooterSocial>
+      </FooterWidth>
     </FooterContainer>
   );
 };
 
 const FooterContainer = styled.div`
+  border-bottom: 3px solid #99ca3b;
+  border-top: 5px solid #86622e;
+  background-color: rgba(153, 202, 59, 0.5);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 8rem;
+
+  a {
+    color: #333;
+  }
+`
+const FooterWidth = styled.div`
+  width: 75%;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   align-items: center;
   text-align: center;
-  width: 34em;
 `
 
 const FooterContact = styled.div`
@@ -46,6 +65,10 @@ const FooterLinks = styled.div`
 
 const FooterSocial = styled.div`
   flex: 1;
+  fisplay: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 5px 15px;
 `
 
 
---
+++ src/components/styled/common.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+export const SLink = styled(Link) `
+  color: #fff;
+`
