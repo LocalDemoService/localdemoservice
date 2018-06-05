b5e40609ffd34803878b72ef2d73df1ea577181d
Will Harrison <willharrison@gmail.com>
2018-06-05 15:08:51 -0700

    fix blog issues

---
+++ src/posts/BlogPage.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { SubpageWrapper, MainColumn, SideColumn, Column, Title, mq } from '../components/styled/common'
+import Contact from '../components/Forms/Contact'
+
+class BlogPage extends Component {
+  render() {
+    const { data } = this.props;
+
+    return (
+      <SubpageWrapper>
+        <MainColumn>
+          <FlexStart>
+            <Title>{data.markdownRemark.frontmatter.title}</Title>
+            <Date>{data.markdownRemark.frontmatter.date}</Date>
+            <div dangerouslySetInnerHTML={{
+              __html: data.markdownRemark.html
+            }} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}></div>
+          </FlexStart>
+        </MainColumn>
+        <SideColumn>
+          <Contact />
+        </SideColumn>
+      </SubpageWrapper>
+    );
+  }
+}
+
+const FlexStart = styled(Column) `
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  @media (max-width: ${mq.small}px) {
+    align-items: flex-end;
+    justify-content: center;
+  }
+`
+
+const Date = styled.div`
+  padding: 25px 0px 25px;
+`;
+
+export default BlogPage;
+
+export const query = graphql`
+  query BlogPostQuery($slug: String!) {
+    markdownRemark(
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
---
+++ src/posts/post.md
@@ -0,0 +1,13 @@
+---
+title: "Welcome to Local Demo Service"
+description: "New blog post"
+date: '2018-05-16T13:39:37-07:00'
+thumbnail: ""
+tags: ['Tags']
+---
+
+Ea sunt nulla fugiat nisi in. Irure id nisi sint officia officia cupidatat. Eu sint est ex excepteur excepteur irure minim occaecat culpa duis non incididunt. Excepteur nisi ex aute enim aute voluptate ut qui ut mollit. Veniam id Lorem mollit aliqua qui do tempor do eiusmod adipisicing excepteur irure voluptate sit.
+
+Ullamco minim amet enim anim amet et incididunt. Veniam do qui culpa excepteur sunt velit cillum irure. Commodo qui tempor ea ex irure qui nostrud.
+
+Cupidatat aliqua labore aute veniam eiusmod quis incididunt eu esse pariatur et ad laboris. Esse ullamco cupidatat sit sint non nisi tempor excepteur irure eu duis non velit qui. Proident dolore cillum Lorem laboris elit amet culpa enim proident cillum eiusmod amet proident. Deserunt eu aliquip exercitation duis esse Lorem commodo culpa nostrud pariatur cillum laboris. Reprehenderit velit culpa mollit quis minim. Labore culpa eiusmod amet eiusmod nulla cillum pariatur ullamco elit aliquip cillum duis. Occaecat aliqua deserunt reprehenderit duis mollit ut duis nulla.
