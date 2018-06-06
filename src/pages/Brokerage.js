f7fb2621235bb0579b5c3a1931275e06f278d7be
LocalDemoService <38673021+LocalDemoService@users.noreply.github.com>
2018-06-06 08:23:08 -0700

    Update Brokerage.js

--- src/pages/Brokerage.js
+++ src/pages/Brokerage.js
@@ -7,7 +7,7 @@ const Brokerage = () => {
     <SubpageWrapper>
       <MainColumn>
         <Title>Brokerage with Local Demo Service</Title>
-        <p>We help products get into new stores. Our team is full time on-site in grocery outlets building relationships with buyers and increasing sales for our brands. Naturally buyers want to take on our product lines because our demos will ensure sales plus we only represent the highest quality items.</p>
+        <p>We help products get into new stores. Our team is on-site full time in grocery outlets building relationships with buyers and increasing sales for our brands. Naturally buyers want to take on our product lines because our demos will ensure sales; plus we only represent the highest quality items.</p>
       </MainColumn>
       <SideColumn>
         <Contact />
