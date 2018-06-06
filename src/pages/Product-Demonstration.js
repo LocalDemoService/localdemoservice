7e54b9738e62e5f2a7367ffa1917de59f30db2a4
LocalDemoService <38673021+LocalDemoService@users.noreply.github.com>
2018-06-06 13:17:03 -0700

    Updated contact text

--- src/pages/Product-Demonstration.js
+++ src/pages/Product-Demonstration.js
@@ -22,7 +22,7 @@ const ProductDemonstration = () => {
         <ul>
           {services.map(service => <li key={service}>{service}</li>)}
         </ul>
-        <p>Interested in hearing more about Product Demonstration? Fill in the ‘Contact Form’ and ask about our Demo program!</p>
+        <p>Interested in hearing Product Demonstration? Call us at 808-722-5492 or fill in the ‘Contact Form’ to the right and ask about our referral program!</p>
       </MainColumn>
       <SideColumn>
         <Contact />
