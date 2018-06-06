970fbfc5e831f489cb8dae10d3624042b2980667
LocalDemoService <38673021+LocalDemoService@users.noreply.github.com>
2018-06-06 13:37:32 -0700

    Updated text

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const About = () => {
       <MainColumn>
         <Title>About Local Demo Service</Title>
         <p>
-          Local Demo Service started in November of 2015 serving local Hawaiian grown beverages at farmers markets to support inter-island sustainability. Hawaii imports approximately 90% of all consumable goods so our mission was to get local products into local peoples hands at all costs. Our local Hawaiian producers expressed a need for demos at grocery outlets so we began sampling Kau coffee, Big Island macadamia nuts, and other local Hawaiian products at Whole Foods and other health food stores. 
+          Local Demo Service started in November of 2015 serving local Hawaiian grown beverages at farmers markets to support inter-island sustainability. Hawaii imports approximately 90% of all consumable goods so our mission was to get local products into local peoples hands at all costs. Our local Hawaiian producers expressed a need for demos at grocery outlets so we began sampling Kau coffee, Big Island macadamia nuts, and other local Hawaiian products at Whole Foods and other health-food stores. 
     </p>
     <p>
     Entering the grocery industry, we found a lack of sustainable trends: Food travels
@@ -18,7 +18,8 @@ const About = () => {
           , Organic food sales only account for a
           <SourceLink href='https://www.ota.com/resources/market-analysis'> mere 5.3% of the food sales</SourceLink> in the United States as of 2017
           , the <SourceLink href='https://www.theguardian.com/environment/2017/mar/13/waste-plastic-food-packaging-recycling-throwaway-culture-dave-hall'> vast majority of food packaging is not recycled</SourceLink>
-          , etc. Our findings led us to our purpose: to support local, Organic, recycled, charity based, healthy, and/or other sustainably focused products to build a more sustainable and forward thinking food economy.
+          , etc. </p>
+    <p>Our findings led us to our purpose: to support sustainably-focused products to build a more sustainable and forward thinking food economy. Our goal is to represent any of the following types of products: <br> -local, <br> Organic <br> recycled <br> charity-based <br> healthy <br> other sustainably focused products
         </p>
       </MainColumn>
       <SideColumn>
