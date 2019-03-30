01139964ca8f33c2b357a5ac601e9827818499cd
LocalDemoService <38673021+LocalDemoService@users.noreply.github.com>
2019-03-29 22:00:44 -0700

    Added info about only taking on select few

--- src/pages/Brokerage.js
+++ src/pages/Brokerage.js
@@ -7,7 +7,7 @@ const Brokerage = () => {
     <SubpageWrapper>
       <MainColumn>
         <Title>Brokerage with Local Demo Service</Title>
-        <p>We help products get into new stores. Our team is on-site full time in grocery outlets building relationships with buyers and increasing sales for our brands. Naturally buyers want to take on our product lines because our demos will ensure sales; plus we only represent the highest quality items.</p>
+        <p>We help products get into new stores. Our team is on-site full time in grocery outlets building relationships with buyers and increasing sales for our brands. Naturally, buyers want to take on our product lines because our demos will ensure sales; plus we only represent the highest quality items. Local Demo Service only takes on a select few clients for Brokerage at a time, so your product needs to be the right fit to become part of our Brokerage Program!</p>
     <p> Interested in hearing Brokerage Rates? Call us at 808-722-5492 or fill in the ‘Contact Form’ to the right and ask about our referral program! </p>
       </MainColumn>
       <SideColumn>
